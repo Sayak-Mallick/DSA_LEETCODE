e78a7ce846b74448b0bec090d430df1851f8020f
sayakmallick4875 <sayak@solivatech.com>
2024-07-30 12:09:53 +0530

    refactor: Refactor palindrome checker and reverse integer functions
    ADD : String to Integer

---
+++ 08-String-to-Integer.js
@@ -0,0 +1,4 @@
+const myAtoi = (s) => {
+  const ans = Number.parseInt(s);
+  return ans <= -2147483648 ? -2147483648 : ans >= 2147483647 ? 2147483647 : ans || 0;
+};
--- 09-Palindrome.js
+++ 09-Palindrome.js
@@ -1,4 +1,4 @@
-var isPalindrome = function(x) {
+const isPalindrome = (x) => {
   let number  = x.toString();
   let reverse = number.split('').reverse().join('');
   return number === reverse;
