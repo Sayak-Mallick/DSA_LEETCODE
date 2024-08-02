8ab5ee3f969317e7c8b2b3ac3cbc27543c6172f8
Sayak Mallick <63100600+Sayak-Mallick@users.noreply.github.com>
2024-08-02 05:57:47 +0530

    Time: 75 ms (52.41%), Space: 52.1 MB (94.08%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+const reverse = (x) => {
+  let number = x.toString().split("").reverse().join("");
+  if(number.includes("-")) {
+    number = parseInt(number) * -1;
+    return number < Math.pow(-2, 31) ? 0 : number;
+  }
+  else {
+    number = parseInt(number);
+    return number > Math.pow(2, 31) - 1 ? 0 : number;
+  }
+}
+
