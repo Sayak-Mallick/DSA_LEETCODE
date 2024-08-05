083aaf170099eafc6e483b80d37c730184bd5e15
Sayak Mallick <63100600+Sayak-Mallick@users.noreply.github.com>
2024-08-05 10:46:46 +0530

    Time: 51 ms (57.48%), Space: 48.4 MB (88.89%) - LeetHub

--- 2556-convert-the-temperature/2556-convert-the-temperature.js
+++ 2556-convert-the-temperature/2556-convert-the-temperature.js
@@ -3,9 +3,7 @@
  * @return {number[]}
  */
 const convertTemperature = (celsius) => {
-    let answer = [];
     let kelvin = (celsius + 273.15).toFixed(5);
     let fahrenheit = (celsius * 1.80 + 32.00).toFixed(5);
-    answer  = [kelvin, fahrenheit];
-    return answer;
+    return ([kelvin, fahrenheit]);
 };
