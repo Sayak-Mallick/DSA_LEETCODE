9f49b0903d2d70ed6ca28670d3b0bd0928d37ca1
Sayak Mallick <63100600+Sayak-Mallick@users.noreply.github.com>
2024-09-02 22:47:14 +0530

    Time: 51 ms (68.82%), Space: 49.4 MB (16.63%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+     for (let i = m; i < m + n; i++) {
+        nums1[i] = nums2[i - m];
+    }
+    
+    // Sort the merged array
+    nums1.sort((a, b) => a - b);
+    
+    // Return the sorted merged array
+    return nums1;
+};
